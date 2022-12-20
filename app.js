400b03127ac4b0476a0e22386b1e997ffe66de41
Iryna Aleksandrova <ivalexie@ukr.net>
2022-12-21 00:35:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const modalCall = $("[data-modal");
+const modalClose = $("[data-close]");
+
+modalCall.on("click", function(event){
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $(modalId).addClass('show'); 
+});
+
+modalClose.on("click", function(event){
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents('.widget');
+
+    modalParent.removeClass('show');
+});
